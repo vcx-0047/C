f6d9f82a269658becd2e8656e137861e118706e7
vcx-0047 <167695472+vcx-0047@users.noreply.github.com>
2024-12-03 22:57:50 +0100

    Update tp2.c

--- tp2.c
+++ tp2.c
@@ -1,64 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-// Fonction pour charger une chaîne de caractères
-char *ChargerChaine(int N) {
-    char *chaine = (char *)malloc((N + 1) * sizeof(char)); // +1 pour le caractère nul
-    if (chaine == NULL) {
-        fprintf(stderr, "Allocation mémoire échouée\n");
-        exit(1); // Gestion d'erreur : sortie du programme en cas d'échec de l'allocation
+char * load_string(int N){ 
+    char *str = (char*)malloc((N + 1) * sizeof(char)); 
+    printf("Insert a  word withe %d letter  at most: ", N); 
+    scanf("%s", str);   
+    return str;
+}
+int length(char *ch){
+    int len = 0; 
+    while (ch[len] != '\0') {
+        len++;
     }
-    printf("Entrez une chaîne de caractères (max %d caractères) : ", N);
-    fgets(chaine, N + 1, stdin); // Lecture de la chaîne avec fgets pour gérer les espaces et éviter les dépassements de tampon
-    chaine[strcspn(chaine, "\n")] = 0; // Supprime le caractère de nouvelle ligne ajouté par fgets
-
-    return chaine;
+    return len;
 }
-
-// Fonction pour calculer la longueur d'une chaîne
-int Longueur(char *ch) {
-    return strlen(ch);
-}
-
-// Procédure pour charger une chaîne dans un tableau
-void ChargerTab(char *p, char Tab[]) {
-    strcpy(Tab, p); // Copie la chaîne dans le tableau
-}
-
-// Procédure pour inverser un tableau de caractères
-void InverserTab(char Tab[], char T[], int m) {
-    for (int i = 0; i < m; i++) {
-        T[i] = Tab[m - 1 - i];
+void load_table(char *p ,char Table[]){
+    int i=0;
+    while (p[i] != '\0') {
+        Table[i]=p[i];
+        i++;
     }
-    T[m] = '\0'; // Ajout du caractère nul pour terminer la chaîne inversée
 }
-
-// Procédure pour afficher un tableau de caractères comme une chaîne
-void AfficherTab(char Tab[], int m) {
-    for (int i = 0; i < m; i++) {
-        printf("%c", Tab[i]);
+void inverse_table(char Table[] , char t[]  ,int m){
+    for(int i=0;i<m;i++){
+       t[m-1-i]=Table[i];
     }
+}
+void display_table(char Table[] ,int m){
+    for(int i=0;i<m;i++){
+        printf("%c",Table[i]);
+    }
+
+}
+int main(){
+    char* ch;
+    unsigned int n;
+    printf("Insert the size of the word: ");
+    scanf("%u", &n);   
+    ch = load_string(n);
+    int m = length(ch);
+    printf("\n this word contain %d letter \n " ,m);
+    char table[m] ,t[m] ;
+    load_table(ch ,table);
+    printf("befor invers \n");
+    display_table(table ,m);
+    inverse_table(table ,t,m);
     printf("\n");
-}
-
-int main() {
-    int N;
-    printf("Entrez la taille maximale de la chaîne : ");
-    scanf("%d", &N);
-    getchar(); // Consomme le  de nouvelle ligne laissé par scanf
-    //N=N+1
-    char *chaine = ChargerChaine(N);
-    int longueur = Longueur(chaine);
-    char Tab[longueur + 1]; // Tableau pour stocker la chaîne
-    char T[longueur + 1]; // Tableau pour stocker la chaîne inversée
-
-    ChargerTab(chaine, Tab);
-    InverserTab(Tab, T, longueur);
-    AfficherTab(Tab, longueur);
-    AfficherTab(T, longueur);
-
-    free(chaine); // Libération de la mémoire allouée dynamiquement
-
+    printf("after invers \n");
+    display_table(t,m);
+    free(ch);
     return 0;
 }
